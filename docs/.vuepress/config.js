0825bd0263ef3b9ce933a0cf5bc9eaf7caea8291
kitaharakasusa <894655469@qq.com>
2019-10-31 17:17:03 +0800

    待完成

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+const sidebar = require('../utils/autoSidebar')
+module.exports = {
+    // base: 'phonix',
+    title: "Phonix's blog",
+    description: "share everyday",
+    markdown: {
+        lineNumbers: true
+    },
+
+    themeConfig: {
+        // 你的GitHub仓库，请正确填写
+        repo: 'https://github.com/Kitaharakasusa',
+        // 自定义仓库链接文字。
+        repoLabel: 'My GitHub',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'blog', 
+              items: [
+                {text: 'JavaScript笔记', link: '/blog/javascript/'},
+                {text: 'Vue笔记', link: '/blog/vue/'},
+                {text: 'React笔记', link: '/blog/react/'},
+                {text: 'Angular笔记', link: '/blog/angular/'},]
+            },
+            { text: 'External', link: 'https://google.com' },
+          ],
+        //   sidebar: [
+        //     '/',
+        //     '/page-a',
+        //     ['/page-b', 'Explicit link text']
+        //   ]
+        sidebar:sidebar,
+        sidebarDepth:2
+    }
+    
+}
